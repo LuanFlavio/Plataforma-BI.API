1fba13eab39180525986c7763aef28147efc679d
LuanFlavio <luanflavio60@gmail.com>
2022-10-25 17:50:48 -0300

    Aplicando autenticação na controller de vendasDiarias

--- PlataformaBI.API/Controllers/VendasDiariasController.cs
+++ PlataformaBI.API/Controllers/VendasDiariasController.cs
@@ -2,19 +2,24 @@
 using Domain;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using PlataformaBI.API.Services;
+using System.Collections.Concurrent;
 
 namespace PlataformaBI.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     [EnableCors("AllowOrigin")]
-    public class VendasDiariasController : Controller
+    public class VendasDiariasController : GsoftController
     {
         private readonly GsoftDbContext _context;
+        //private readonly ConcurrentDictionary<string, Session> sessions;
 
-        public VendasDiariasController(GsoftDbContext context)
+        public VendasDiariasController(GsoftDbContext context, ConcurrentDictionary<string, Session> sessions)
+            :base(sessions)
         {
             _context = context;
+            //this.sessions = sessions;
         }
 
         /// <summary>
@@ -23,6 +28,14 @@ namespace PlataformaBI.API.Controllers
         [HttpGet]
         public IActionResult Get([FromQuery] PageParams<VendasDiarias> pageParams)
         {
+            if(!UserAuthenticated)
+                return Unauthorized();
+
+            if (pageParams.Termo == null || pageParams.Termo.Empresa == 0)
+            {
+                return BadRequest("MissingParams");
+            }
+
             var vendasDiarias = BuscarVendas(pageParams);
 
             if (vendasDiarias == null)
@@ -34,19 +47,23 @@ namespace PlataformaBI.API.Controllers
         }
 
         [NonAction]
-        public IActionResult BuscarVendas(PageParams<VendasDiarias> pageParams)
+        public PageList<VendasDiarias> BuscarVendas(PageParams<VendasDiarias> pageParams)
         {
-            if(pageParams.Termo == null || pageParams.Termo.Empresa == 0)
+
+            if(pageParams.Termo.Data == null)
             {
-                return BadRequest("MissingParams");
+                pageParams.Termo.Data = DateTime.Today.AddDays(-1);
             }
+
+            //DATA FINAL = DateTime.Today.AddDays(-1);
+            //DATA FINAL = DateTime.Today.AddDays(-1);
 
             IEnumerable<VendasDiarias> vendasDiarias = _context
                                         .vendasDiarias
                                         .Where(p => p.Empresa.Equals(pageParams.Termo.Empresa) && p.Data.Equals(pageParams.Termo.Data))
                                         .ToArray();
 
-            return Ok(PageList<VendasDiarias>.CreateAsync(vendasDiarias, pageParams.PageNumber, pageParams.PageSize));
+            return PageList<VendasDiarias>.CreateAsync(vendasDiarias, pageParams.PageNumber, pageParams.PageSize);
         }
     }
 }
