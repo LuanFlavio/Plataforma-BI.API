3646fe24cb0372a0d103e7d09a00f37ba79387d7
LuanFlavio <luanflavio60@gmail.com>
2022-10-27 17:53:33 -0300

    Replicando a controller de vendasDiárias e adaptando para vendasMensais

---
+++ Domain/VendasMensais.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    [Table("VendasMensal")]
+    public class VendasMensais : Vendas
+    {
+    }
+
+    public class VendasMensaisParam
+    {
+        public int? ID { get; set; }
+        public int? MesDoAno { get; set; }
+        public int? Ano { get; set; }
+    }
+}
---
+++ PlataformaBI.API/Controllers/VendasMensaisController.cs
@@ -0,0 +1,78 @@
+﻿using DataAccess;
+using Domain;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using PlataformaBI.API.Services;
+using PlataformaBI.API.Utils;
+using System.Collections.Concurrent;
+
+namespace PlataformaBI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    public class VendasMensaisController : GsoftController
+    {
+        private readonly GsoftDbContext _context;
+        //private readonly ConcurrentDictionary<string, Session> sessions;
+
+        public VendasMensaisController(GsoftDbContext context, ConcurrentDictionary<string, Session> sessions)
+            : base(sessions)
+        {
+            _context = context;
+            //this.sessions = sessions;
+        }
+
+        /// <summary>
+        /// Retorna os dados do vendasMensais conforme o CNPJ da empresa
+        /// </summary>
+        [HttpGet]
+        public IActionResult Get([FromQuery] PageParams<VendasMensaisParam> pageParams)
+        {
+            if (!UserAuthenticated)
+                return Unauthorized();
+
+            var vendasMensais = BuscarVendas(pageParams);
+
+            if (vendasMensais == null)
+            {
+                return NoContent();
+            }
+
+            Response.AddPagination(vendasMensais.CurrentPage, vendasMensais.PageSize, vendasMensais.TotalCount, vendasMensais.TatalPages);
+
+            return Ok(vendasMensais);
+        }
+
+        [NonAction]
+        public PageList<VendasMensais> BuscarVendas(PageParams<VendasMensaisParam> pageParams)
+        {
+            IEnumerable<VendasMensais> vendasMensais;
+
+            if (pageParams.Termo != null)
+            {
+                //DATA FINAL = DateTime.Today.AddDays(-1);
+                //DATA FINAL = DateTime.Today.AddDays(-1);
+
+                vendasMensais = _context
+                    .vendasMensal
+                    .Where(p =>
+                        (p.Empresa.Equals(Session.usuarioLogado.Empresa)) &&
+                        (pageParams.Termo.Ano != null ? p.Ano.Equals(pageParams.Termo.Ano) : true) &&
+                        (pageParams.Termo.MesDoAno != null ? p.MesDoAno.Equals(pageParams.Termo.MesDoAno) : true) &&
+                        (pageParams.Termo.ID != null ? p.ID.Equals(pageParams.Termo.ID) : true)
+                    )
+                    .ToArray();
+            }
+            else
+            {
+                vendasMensais = _context
+                        .vendasMensal
+                        .Where(p => p.Empresa.Equals(Session.usuarioLogado.Empresa))
+                        .ToArray();
+            }
+
+            return PageList<VendasMensais>.CreateAsync(vendasMensais, pageParams.PageNumber, pageParams.PageSize);
+        }
+    }
+}
